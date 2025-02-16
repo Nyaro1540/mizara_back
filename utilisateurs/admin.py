585bb6de6b31a657d74a6cfa94ecc1f4c8fa0139
nyaro1540 <nyaro15400@gmail.com>
2025-02-16 15:56:16 +0300

    affichage des client dans Admin

--- utilisateurs/admin.py
+++ utilisateurs/admin.py
@@ -5,18 +5,22 @@ from .models import User, Utilisateurs
 class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ('nom_complet', 'prenom', 'numero_telephone', 'email', 'lieu_habitation', 'role', 'is_active', 'is_staff')
+    list_filter = ('role', 'is_active', 'is_staff')
     fieldsets = (
         (None, {'fields': ('numero_telephone', 'password')}),
         ('Informations personnelles', {'fields': ('nom_complet', 'prenom', 'email', 'lieu_habitation')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     )
+    # Exclude username field from add form
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('numero_telephone', 'nom_complet', 'prenom', 'email', 'lieu_habitation', 'password1', 'password2')}
-        ),
+            'fields': ('numero_telephone', 'nom_complet', 'prenom', 'email', 'lieu_habitation', 'password1', 'password2'),
+        }),
     )
     search_fields = ('numero_telephone', 'nom_complet', 'prenom', 'email')
     ordering = ('nom_complet',)
+    list_per_page = 25
+    show_full_result_count = True
 
 admin.site.register(User, CustomUserAdmin)
